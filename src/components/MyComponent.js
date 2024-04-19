06791d1a60e8809e4b4a388a331830917506c289
nam phung <oklegend1234@gmail.com>
2024-04-19 20:26:57 +0700

    component reactjs

---
+++ src/components/MyComponent.js
@@ -0,0 +1,17 @@
+// class component
+// function component
+import React from 'react'
+
+class MyComponent extends React.Component {
+  // jsx
+  render() {
+    return (
+      <div>
+        My first Component
+        {Math.random()}
+      </div>
+    )
+  }
+}
+
+export default MyComponent
