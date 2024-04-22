26e359f39e49bf73c71dde21350408f4be258849
nam phung <oklegend1234@gmail.com>
2024-04-22 14:18:43 +0700

    useEffect hook

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,33 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './DisplayInfo.scss'
-
-// class DisplayInfo extends React.Component {
-//   render() {
-//     console.log('call me render')
-//     const { listUsers } = this.props
-//     return (
-//       <div className="display-info-container">
-//         {true && (
-//           <>
-//             {listUsers.map((user) => {
-//               // console.log(user)
-//               return (
-//                 <div key={user.id} className={user.age > 18 ? 'green' : 'red'}>
-//                   <div>My name is {user.name} </div>
-//                   <div>I'm {user.age} years old</div>
-//                   <button onClick={() => this.props.handleDeleteUser(user.id)}>
-//                     Delete
-//                   </button>
-//                   <hr />
-//                 </div>
-//               )
-//             })}
-//           </>
-//         )}
-//       </div>
-//     )
-//   }
-// }
 
 const DisplayInfo = (props) => {
   const { listUsers } = props
@@ -36,6 +8,18 @@ const DisplayInfo = (props) => {
   const handleShowHideListUser = () => {
     setIsShowHideListUser(!isShowHideListUser)
   }
+
+  console.log('>>> call me render')
+
+  useEffect(() => {
+    if (listUsers.length === 0) {
+      alert('You deleted all users')
+    }
+    setTimeout(() => {
+      document.title = 'test'
+    }, 3000)
+    console.log('call me useEffect')
+  }, [listUsers])
 
   return (
     <div className="display-info-container">
