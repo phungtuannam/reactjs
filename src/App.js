4eb35ba1fa600436a7a7fe88f9e0d113c5e8b7d4
nam phung <oklegend1234@gmail.com>
2024-04-23 20:11:30 +0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.scss'
 import React from 'react'
 import Header from './components/Header/Header'
+import { Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>test link</div>
+      <button>
+        <Link to="/users">go to User page</Link>
+      </button>
+      <button>
+        <Link to="/admins">go to Admin page</Link>
+      </button>
     </div>
   )
 }
