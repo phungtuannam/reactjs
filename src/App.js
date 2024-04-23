81047efb54b69e0214825895cc81860685ca2a84
nam phung <oklegend1234@gmail.com>
2024-04-23 19:30:35 +0700

    header bootstrap

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
-import logo from './logo.svg'
 import './App.scss'
-import { useDispatch, useSelector } from 'react-redux'
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction'
-import MyComponent from './components/MyComponent'
 import React from 'react'
+import Header from './components/Header/Header'
 
 const App = () => {
   return (
     <div className="app-container">
-      <p>Hello world</p>
-      <MyComponent></MyComponent>
+      <Header />
     </div>
   )
 }
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count)
-//   const dispatch = useDispatch()
-
-//   return (
-//     <div>
-//       <p>Hello world</p>
-//       <MyComponent></MyComponent>
-//     </div>
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>Hello world</p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   )
-// }
 
 export default App
