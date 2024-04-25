eccb4c33e9e904595d17f8fce56dbf8f634cb656
nam phung <oklegend1234@gmail.com>
2024-04-25 16:48:55 +0700

    design sidebar

---
+++ src/components/Admin/Sidebar.js
@@ -0,0 +1,98 @@
+import {
+  ProSidebar,
+  Menu,
+  MenuItem,
+  SubMenu,
+  SidebarHeader,
+  SidebarFooter,
+  SidebarContent,
+} from 'react-pro-sidebar'
+import {
+  FaTachometerAlt,
+  FaGem,
+  FaGithub,
+  FaRegLaughWink,
+} from 'react-icons/fa'
+import sidebarBg from '../../assets/bg2.jpg'
+
+const Sidebar = ({ image, collapsed, toggled, handleToggleSidebar }) => {
+  return (
+    <>
+      <ProSidebar
+        image={sidebarBg}
+        collapsed={collapsed}
+        toggled={toggled}
+        breakPoint="md"
+        onToggle={handleToggleSidebar}
+      >
+        <SidebarHeader>
+          <div
+            style={{
+              padding: '24px',
+              textTransform: 'uppercase',
+              fontWeight: 'bold',
+              fontSize: 14,
+              letterSpacing: '1px',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+            }}
+          >
+            Nam Phung
+          </div>
+        </SidebarHeader>
+
+        <SidebarContent>
+          <Menu iconShape="circle">
+            <MenuItem
+              icon={<FaTachometerAlt />}
+              suffix={<span className="badge red">New</span>}
+            >
+              dashboard
+            </MenuItem>
+            <MenuItem icon={<FaGem />}>components</MenuItem>
+          </Menu>
+          <Menu iconShape="circle">
+            <SubMenu
+              suffix={<span className="badge yellow">3</span>}
+              icon={<FaRegLaughWink />}
+            >
+              <MenuItem> 1</MenuItem>
+              <MenuItem> 2</MenuItem>
+              <MenuItem> 3</MenuItem>
+            </SubMenu>
+          </Menu>
+        </SidebarContent>
+
+        <SidebarFooter style={{ textAlign: 'center' }}>
+          <div
+            className="sidebar-btn-wrapper"
+            style={{
+              padding: '20px 24px',
+            }}
+          >
+            <a
+              href="https://github.com/azouaoui-med/react-pro-sidebar"
+              target="_blank"
+              className="sidebar-btn"
+              rel="noopener noreferrer"
+            >
+              <FaGithub />
+              <span
+                style={{
+                  whiteSpace: 'nowrap',
+                  textOverflow: 'ellipsis',
+                  overflow: 'hidden',
+                }}
+              >
+                View Source
+              </span>
+            </a>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
+    </>
+  )
+}
+
+export default Sidebar
